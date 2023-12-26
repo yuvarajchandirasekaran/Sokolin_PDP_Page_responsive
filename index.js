a94c8705257a0a7a500cd71d17197aea5b3f4187
yuvarajchandirasekaran <yuvarajcdckap@gmail.com>
2023-12-26 10:20:57 +0530

    container was set for cart-detail-view and tab-functionality

--- index.js
+++ index.js
@@ -198,3 +198,24 @@ $(document).ready(function () {
   });
 
 });
+
+
+$(document).ready(function() {
+  $("#news-slider").owlCarousel({
+      items : 3,
+      itemsDesktop:[1199,3],
+      itemsDesktopSmall:[980,2],
+      itemsMobile : [600,1],
+      navigation:true,
+      navigationText:["",""],
+      pagination:true,
+      autoPlay:true
+  });
+
+  if(typeof owlCarousel === 'function') {
+    $("#owl-demo").owlCarousel({
+      navigation : true
+    });
+  }
+
+});
